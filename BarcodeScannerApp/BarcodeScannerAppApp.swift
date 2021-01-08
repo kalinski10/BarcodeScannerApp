115f0d4186468a47549f12f4fe35221418b5b5ef
kalin balabnov <kalin_dude10@live.com>
2021-01-08 09:17:05 +0000

    Initial Commit

---
+++ BarcodeScannerApp/BarcodeScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerAppApp.swift
+//  BarcodeScannerApp
+//
+//  Created by Kalin Balabanov on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
